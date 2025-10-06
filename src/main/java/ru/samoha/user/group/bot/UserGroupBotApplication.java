2895d280518a7d1226ae27408e11bd95b5662003
isamoilov <isamoilov@iitdgroup.ru>
2025-10-06 12:05:03 +0300

    Initial commit

---
+++ src/main/java/ru/samoha/user/group/bot/UserGroupBotApplication.java
@@ -0,0 +1,13 @@
+package ru.samoha.user.group.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserGroupBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserGroupBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/iitdgroup/user/group/bot/UserGroupBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iitdgroup.user.group.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserGroupBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
